4cde32b44cc511e1c4d7a54159ebd681f8d2d382
Jared Clason <jclason1@gmail.com>
2012-02-24 20:21:24 -0500

    Refactored script tag creation into getScriptTag(string), adding logging through nLog. Added imported method to make relative file path

--- gvAddReference/MainWindow.xaml.cs
+++ gvAddReference/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using NLog;
 
 namespace gvAddReference
 {
@@ -22,6 +25,8 @@ namespace gvAddReference
     public partial class MainWindow : Window
     {
         Boolean filesChanged = false;
+        List<String> changeLog = new List<String>();
+        private static Logger logger = LogManager.GetCurrentClassLogger();
 
         public MainWindow()
         {
@@ -32,17 +37,6 @@ namespace gvAddReference
         {
             String filePath = filePathBox.Text;
             String pagePath = pagePathBox.Text;
-            String htmlTag = "";
-
-            // Set the html tag string based on the type selected
-            if (jsRadioButton.IsChecked.Value)
-            {
-                htmlTag = "<script type=\"text/javascript\" src=\"" + filePath + "\" ></script>";
-            }
-            else
-            {
-                htmlTag = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + filePath + "\" />";
-            }
 
             // For each .aspx or html file in the selected pagePath, or the single file selected, insert the tag
             // Read the file as one string.
@@ -52,15 +46,17 @@ namespace gvAddReference
                 if(!File.Exists(filePath))
                 {
                     // File does not exist error message
+                    return;
                 }
                 else if (Directory.Exists(filePath))
                 {
                     // Pick a file not a directory for the script message
+                    return;
                 }
 
                 statusText.Text = "Looking for pages...";
                 // Check for aspx or html pages in the directory, or the file
-                System.Text.RegularExpressions.Regex fileExtension = new System.Text.RegularExpressions.Regex("^.+(.aspx|.ASPX|.html|.HTML)$");
+                Regex fileExtension = new Regex("^.+(.aspx|.ASPX|.html|.HTML)$");
                 if (!directoryCheckBox.IsChecked.Value)
                 {
                     if (File.Exists(pagePath))
@@ -68,7 +64,7 @@ namespace gvAddReference
                         if (fileExtension.IsMatch(pagePath))
                         {
                             statusText.Text = "Adding script to page...";
-                            addTagToPage(htmlTag, pagePath);
+                            addTagToPage(filePath, pagePath);
                         }
                     }
                 }
@@ -80,12 +76,12 @@ namespace gvAddReference
                     {
                         statusText.Text = "Looking for files in directory...";
                         // Call add tag to page for each page file
-                        processDirectory(pagePath, htmlTag, fileExtension);
+                        processDirectory(pagePath, filePath, fileExtension);
 
                         // Need to fix the tag to use a different relative url for subdir's
                         foreach (string dir in Directory.GetDirectories(pagePath))
                         {
-                            processDirectory(dir, htmlTag, fileExtension);
+                            processDirectory(dir, filePath, fileExtension);
                         }
                     }
                     else
@@ -111,30 +107,62 @@ namespace gvAddReference
             //}
         }
 
-        private void processDirectory(String pagePath, String htmlTag, System.Text.RegularExpressions.Regex fileExtension)
+        private String getScriptTag(String filePath)
+        {
+            String htmlTag;
+            // Set the html tag string based on the type selected
+            if (jsRadioButton.IsChecked.Value)
+            {
+                htmlTag = "<script type=\"text/javascript\" src=\"" + filePath + "\" ></script>";
+            }
+            else
+            {
+                htmlTag = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + filePath + "\" />";
+            }
+            return htmlTag;
+        }
+
+        private void processDirectory(String pagePath, String filePath, Regex fileExtension)
         {
             string[] fileEntries = Directory.GetFiles(pagePath);
             foreach (string fileName in fileEntries)
             {
                 if (fileExtension.IsMatch(fileName))
                 {
-                    addTagToPage(htmlTag, fileName);
+                    addTagToPage(filePath, fileName);
                 }
             }
         }
 
         // Adds the script tag in the string to the page just before the closing head tag
-        private void addTagToPage(String scriptTag, String fileName)
+        private void addTagToPage(String filePath, String fileName)
         {
+            String scriptTag;
             // Read the file for manipulation
             System.IO.StreamReader readFile = new System.IO.StreamReader(fileName);
             var allLines = File.ReadAllLines(fileName).ToList();
             readFile.Close();
 
+            // Buffer for holding the relative path
+            StringBuilder str = new StringBuilder(260);
+            // Make the path we add in the script a relative path to the page
+            // Function imported from dll at the bottom of this class
+            bool madeRelative = PathRelativePathTo(str, fileName, FileAttributes.Normal, filePath, FileAttributes.Normal);
+            if (madeRelative)
+                scriptTag = getScriptTag(str.ToString());
+            else
+            {
+                // Uhh, problem getting the relative path
+                // just use the script fileName for the tag
+                scriptTag = getScriptTag(fileName);
+            }
+            
             int index = allLines.FindIndex(i => i.Contains("</head>"));
             allLines.Insert(index, scriptTag);
             File.WriteAllLines(fileName, allLines.ToArray());
             filesChanged = true;
+            logger.Info("File changed: " + fileName + " | Script reference added: " + filePath + " | Relative reference: " + str.ToString() +
+                 " | HTML Tag: " + scriptTag);
         }
 
         private void filePathButton_Click(object sender, RoutedEventArgs e)
@@ -167,5 +195,14 @@ namespace gvAddReference
                 pagePathBox.Text = ofd.FileName;
             }
         }
+
+        [DllImport("shlwapi.dll", CharSet = CharSet.Auto)]
+        static extern bool PathRelativePathTo(
+             [Out] StringBuilder pszPath,
+             [In] string pszFrom,
+             [In] FileAttributes dwAttrFrom,
+             [In] string pszTo,
+             [In] FileAttributes dwAttrTo
+        );
     }
 }
