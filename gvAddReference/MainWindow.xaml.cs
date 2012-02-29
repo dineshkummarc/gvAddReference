d1e7252b59bbecff3d21b629a8cacdfd6cd4b559
Jared Clason <jclason1@gmail.com>
2012-02-28 19:39:02 -0500

    Fixed the check for "Inherits BasePage" to read the .vb file and check for it there

--- gvAddReference/MainWindow.xaml.cs
+++ gvAddReference/MainWindow.xaml.cs
@@ -44,7 +44,6 @@ namespace gvAddReference
             // Read the file as one string.
             //try
             //{
-                statusText.Text = "Checking script file...";
                 if(!File.Exists(filePath))
                 {
                     // File does not exist error message
@@ -56,7 +55,6 @@ namespace gvAddReference
                     return;
                 }
 
-                statusText.Text = "Looking for pages...";
                 // Check for aspx or html pages in the directory, or the file
                 Regex fileExtension = new Regex("^.+(.aspx|.ASPX|.html|.HTML)$");
                 if (!directoryCheckBox.IsChecked.Value)
@@ -65,18 +63,16 @@ namespace gvAddReference
                     {
                         if (fileExtension.IsMatch(pagePath))
                         {
-                            statusText.Text = "Adding script to page...";
                             addTagToPage(filePath, pagePath);
                         }
                     }
                 }
                 else 
                 {
-                    // Use just the directory path
+                    // Use just the directory path and process all directories and files
                     pagePath = pagePath.Substring(0, pagePath.LastIndexOf('\\'));
                     if (Directory.Exists(pagePath))
                     {
-                        statusText.Text = "Looking for files in directory...";
                         // Call add tag to page for each page file
                         processDirectory(pagePath, filePath, fileExtension);
 
@@ -105,8 +101,8 @@ namespace gvAddReference
 
                 // Based on if filesChanged, give the user a message
                 if (filesChanged)
-                    statusText.Text = "References added successfully!";
-                else statusText.Text = "No changes were made to any files";
+                    statusText.Text = "References added successfully! See ChangeLog.txt";
+                else statusText.Text = "No changes were made to any files. See Log.txt";
             //}
             //catch (Exception ex)
             //{
@@ -132,8 +128,7 @@ namespace gvAddReference
                 htmlTag = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + filePath + "\" />";
             }
             // Local paths use backslash, web paths use forward slash so convert
-            htmlTag.Replace("\\", "/");
-            return htmlTag;
+            return htmlTag.Replace("\\", "/");
         }
 
         // Calls addTagToPage on every file in the directory that matches the regex
@@ -152,6 +147,7 @@ namespace gvAddReference
         // Adds the script tag in the string to the page just before the closing head tag
         private void addTagToPage(String scriptPath, String fileName)
         {
+
             String scriptTag;
             // Read the file for manipulation
             System.IO.StreamReader readFile = new System.IO.StreamReader(fileName);
@@ -187,14 +183,22 @@ namespace gvAddReference
                 }
             }
 
-            // If this page inherits from the base page, we could add the script there
-            // So don't add the reference to it
-            int line = 0;
-            if ((line = allLines.FindIndex(i => i.Contains("Inherits BasePage"))) > 0)
+            if (File.Exists(fileName + ".vb"))
             {
-                logger.Debug("File " + fileName + " inherits from the BasePage, no changes were made");
-                logger.Info("File: " + fileName + " | On Line number: " + line + " | Page inherits from BasePage | No changes were made");
-                return;
+                // Read the file's codebehind file to check if it inherits from the Base Page
+                System.IO.StreamReader codeFile = new System.IO.StreamReader(fileName + ".vb");
+                var codeLines = File.ReadAllLines(fileName).ToList();
+                codeFile.Close();
+
+                // If this page inherits from the base page, we can add the script there
+                // so don't add another reference to it
+                int line = 0;
+                if ((line = codeLines.FindIndex(i => i.Contains("Inherits BasePage"))) > 0)
+                {
+                    logger.Debug("File " + fileName + " inherits from the BasePage, no changes were made");
+                    logger.Info("File: " + fileName + " | On Line number: " + line + " | Page inherits from BasePage | No changes were made");
+                    return;
+                }
             }
 
             //Insert the script tag just before the closing head tag
@@ -248,5 +252,11 @@ namespace gvAddReference
              [In] string pszTo,
              [In] FileAttributes dwAttrTo
         );
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Should this close or clear the form? bad name
+            gvMainWindow.Close();
+        }
     }
 }
